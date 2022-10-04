490c910bf44251b5a56cd616466b982b2e193d81
wushengjie.wsj <wushengjie.wsj@bytedance.com>
2022-10-04 19:43:13 +0800

    test

--- m_moke/main.go
+++ m_moke/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func navFilter() string {
+	return "{\"dimension\":\"department\",\"node_id\":\"7130074483159090732\",\"is_strand_id_in\":true}"
+}
+
+type request struct {
+	Dimension    string `json:"dimension"`
+	NodeId       string `json:"node_id"`
+	IsStrandIdIn bool   `json:"is_strand_id_in"`
+}
+
+func myEnumOption(req string) {
+	nav := request{}
+
+	err := json.Unmarshal([]byte(req), &nav)
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	fmt.Println(nav)
+}
 
 func main() {
-	fmt.Println("hello world")
+	myEnumOption(navFilter())
 }
